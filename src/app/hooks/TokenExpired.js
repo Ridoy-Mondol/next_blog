bc55567d22206e44b49b4afd8d381a42e56803fc
Ridoy Mondol <ridoymondol140@gmail.com>
2024-05-18 13:20:29 +0600

    solved token validation

--- src/app/hooks/TokenExpired.js
+++ src/app/hooks/TokenExpired.js
@@ -1,48 +1,51 @@
+"use client";
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
+
+const TokenExpired = (WrappedComponent) => {
+  const TokenExpiredComponent = (props) => {
+    const router = useRouter();
+    const [tokenValid, setTokenValid] = useState(true);
+
+    useEffect(() => {
+      const token = (typeof localStorage !== 'undefined') && localStorage.getItem('token');
+      if (token) {
+        setTokenValid(true);
+        router.replace('/');
+      }
+    }, [router]);
+
+    return tokenValid ? <WrappedComponent {...props} /> : null;
+  };
+
+  TokenExpiredComponent.displayName = `TokenExpired(${WrappedComponent.displayName || WrappedComponent.name || 'Component'})`;
+
+  return TokenExpiredComponent;
+};
+
+export default TokenExpired;
+
+
+
+
 // "use client"
 // import { useRouter } from 'next/navigation';
 // import { useEffect } from 'react';
 
 // const TokenExpired = (WrappedComponent) => {
-//   const TokenExpiredComponent = (props) => {
+//   return (props) => {
 //     const router = useRouter();
 //     const token = (typeof localStorage !== 'undefined') && localStorage.getItem('token');
 
 //     useEffect(() => {
-//       if (!token) {
+//       if (token) {
 //         router.replace('/');
 //       }
 //     }, [token, router]);
 
 //     return <WrappedComponent {...props} />;
 //   };
-
-//   TokenExpiredComponent.displayName = `TokenExpired(${WrappedComponent.displayName || WrappedComponent.name || 'Component'})`;
-
-//   return TokenExpiredComponent;
 // };
 
 // export default TokenExpired;
-
-
-
-"use client"
-import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
-
-const TokenExpired = (WrappedComponent) => {
-  return (props) => {
-    const router = useRouter();
-    const token = (typeof localStorage !== 'undefined') && localStorage.getItem('token');
-
-    useEffect(() => {
-      if (token) {
-        router.replace('/');
-      }
-    }, [token, router]);
-
-    return <WrappedComponent {...props} />;
-  };
-};
-
-export default TokenExpired;
 
--- src/app/hooks/TokenValidation.js
+++ src/app/hooks/TokenValidation.js
@@ -1,9 +1,38 @@
+"use client";
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
+
+const TokenValidation = (WrappedComponent) => {
+  const TokenExpiredComponent = (props) => {
+    const router = useRouter();
+    const [tokenValid, setTokenValid] = useState(true);
+
+    useEffect(() => {
+      const token = (typeof localStorage !== 'undefined') && localStorage.getItem('token');
+      if (!token) {
+        setTokenValid(false);
+        router.replace('/signup');
+      }
+    }, [router]);
+
+    return tokenValid ? <WrappedComponent {...props} /> : null;
+  };
+
+  TokenExpiredComponent.displayName = `TokenExpired(${WrappedComponent.displayName || WrappedComponent.name || 'Component'})`;
+
+  return TokenExpiredComponent;
+};
+
+export default TokenValidation;
+
+
+
 // "use client"
 // import { useRouter } from 'next/navigation';
 // import { useEffect } from 'react';
 
 // const TokenValidation = (WrappedComponent) => {
-//   const TokenValidationComponent = (props) => {
+//   return (props) => {
 //     const router = useRouter();
 //     const token = (typeof localStorage !== 'undefined') && localStorage.getItem('token');
 
@@ -15,33 +44,7 @@
 
 //     return <WrappedComponent {...props} />;
 //   };
-
-//   TokenValidationComponent.displayName = `TokenValidation(${WrappedComponent.displayName || WrappedComponent.name || 'Component'})`;
-
-//   return TokenValidationComponent;
 // };
 
 // export default TokenValidation;
-
-
-"use client"
-import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
-
-const TokenValidation = (WrappedComponent) => {
-  return (props) => {
-    const router = useRouter();
-    const token = (typeof localStorage !== 'undefined') && localStorage.getItem('token');
-
-    useEffect(() => {
-      if (!token) {
-        router.replace('/signup');
-      }
-    }, [token, router]);
-
-    return <WrappedComponent {...props} />;
-  };
-};
-
-export default TokenValidation;
 
