f8f61a0d097293438b452aad1eed392a06f2604a
Ridoy Mondol <ridoymondol140@gmail.com>
2024-07-06 14:08:14 +0600

    debugging

--- src/app/api/users/blog/[id]/route.js
+++ src/app/api/users/blog/[id]/route.js
@@ -5,6 +5,7 @@ import user from "@/models/signupModel";
 import cloudinary from '@/utils/cloudinary';
 import {redisClient, connectRedis} from "@/utils/redis"
 import jwt from 'jsonwebtoken';
+import { Readable } from 'stream';
 
 
 const secretKey = process.env.JWT_SECRET_KEY;
@@ -62,6 +63,110 @@ export async function DELETE(request, content) {
 }
 
 
+// export async function PATCH(request, content) {
+//   let success = false;
+//   const id = content.params.id; 
+//   const ids = id.split(',').map(id => id);
+
+//   try {
+//     const data = await request.formData();
+//     const updates = {};
+//     const token = request.headers.get('Authorization')?.split(' ')[1];
+//     const decodedToken = jwt.verify(token, secretKey);
+//     const userId = decodedToken.userId;
+//     const author = await user.findOne({_id: userId});
+
+//     if (data.has('title')) {
+//       updates.title = data.get('title');
+//       success = true;
+//     }
+//     if (data.has('blog')) {
+//       updates.blog = data.get('blog');
+//       success = true; 
+//     }
+//     if (data.has('category')) {
+//       updates.category = data.get('category');
+//       success = true;
+//     }
+//     if (data.has('image1')) {
+//       const blogPosts = await BlogPost.find({ _id: { $in: ids }, 'user.author': userId });
+//       if (blogPosts.image1) {
+//         await cloudinary.uploader.destroy(user.image1);
+//       }
+
+//       // if (new Blob([blog]).size > 500000) {
+//       //   return new NextResponse(
+//       //     JSON.stringify({ success: false, error: 'Blog content is too large.' }),
+//       //     { status: 413 } 
+//       //   );
+//       // }
+
+//       const image1File = data.get('image1');
+//       const buffer1 = await image1File.arrayBuffer();
+//       const image1 = Buffer.from(new Uint8Array(buffer1));
+
+//       // if (image1.length > 2000000) {
+//       //   return new NextResponse(
+//       //     JSON.stringify({ success: false, error: 'Image is too large.' }),
+//       //     { status: 413 } 
+//       //   );
+//       // }
+
+//       const imageUrl1 = await uploadToCloudinary(image1, image1File.name, image1File.type);
+//       updates.image1 = imageUrl1;
+//       success = true; 
+//     }
+
+//     if (author) {
+//       updates.user = {
+//         name: author.name,
+//         profileImage: author.profileImage ? author.profileImage : null,
+//         author: author._id
+//       };
+//       success = true;
+//     }
+
+//     if (success) {
+//       await connectDB();
+//       await BlogPost.updateMany({ _id: { $in: ids }, 'user.author': userId }, { $set: updates });
+//       await redisClient.del('blog_posts');
+//       await redisClient.del(`single_blog_posts${id}`);
+//       return NextResponse.json({ success });
+//     } else {
+//       return NextResponse.json({ success: false, message: 'No valid fields to update' });
+//     }
+//   } catch (error) {
+//     console.error('Error updating post:', error);
+//     return NextResponse.json({ success: false, error: error.message });
+//   } 
+// }
+
+// function uploadToCloudinary(buffer, fileName, mimeType) {
+//   return new Promise((resolve, reject) => {
+//     const stream = cloudinary.uploader.upload_stream(
+//       {
+//         folder: "blog",
+//         filename_override: fileName,
+//         format: mimeType.split('/').pop(),
+//       },
+//       (error, result) => {
+//         if (error) {
+//           console.error('Error uploading file to Cloudinary:', error);
+//           return reject(error);
+//         }
+//         resolve(result.secure_url);
+//       }
+//     );
+
+//     stream.end(buffer);
+//   });
+// }
+
+
+
+
+
+
 export async function PATCH(request, content) {
   let success = false;
   const id = content.params.id; 
@@ -79,40 +184,25 @@ export async function PATCH(request, content) {
       updates.title = data.get('title');
       success = true;
     }
-    if (data.has('blog')) {
-      updates.blog = data.get('blog');
-      success = true; 
-    }
+
     if (data.has('category')) {
       updates.category = data.get('category');
       success = true;
     }
+
     if (data.has('image1')) {
-      const blogPosts = await BlogPost.find({ _id: { $in: ids }, 'user.author': userId });
-      if (blogPosts.image1) {
-        await cloudinary.uploader.destroy(user.image1);
-      }
-
-      // if (new Blob([blog]).size > 500000) {
-      //   return new NextResponse(
-      //     JSON.stringify({ success: false, error: 'Blog content is too large.' }),
-      //     { status: 413 } 
-      //   );
-      // }
-
       const image1File = data.get('image1');
       const buffer1 = await image1File.arrayBuffer();
       const image1 = Buffer.from(new Uint8Array(buffer1));
 
-      // if (image1.length > 2000000) {
-      //   return new NextResponse(
-      //     JSON.stringify({ success: false, error: 'Image is too large.' }),
-      //     { status: 413 } 
-      //   );
-      // }
-
       const imageUrl1 = await uploadToCloudinary(image1, image1File.name, image1File.type);
       updates.image1 = imageUrl1;
+      success = true; 
+    }
+
+    if (data.has('blog')) {
+      const blogStream = data.get('blog').stream();
+      updates.blog = await saveBlogContent(blogStream); // Save blog content as readable stream
       success = true; 
     }
 
@@ -140,7 +230,7 @@ export async function PATCH(request, content) {
   } 
 }
 
-function uploadToCloudinary(buffer, fileName, mimeType) {
+async function uploadToCloudinary(buffer, fileName, mimeType) {
   return new Promise((resolve, reject) => {
     const stream = cloudinary.uploader.upload_stream(
       {
@@ -158,5 +248,21 @@ function uploadToCloudinary(buffer, fileName, mimeType) {
     );
 
     stream.end(buffer);
+  });
+}
+
+async function saveBlogContent(blogStream) {
+  return new Promise((resolve, reject) => {
+    const chunks = [];
+    blogStream.on('data', chunk => {
+      chunks.push(chunk);
+    });
+    blogStream.on('end', () => {
+      const buffer = Buffer.concat(chunks);
+      resolve(buffer.toString());
+    });
+    blogStream.on('error', error => {
+      reject(error);
+    });
   });
 }
--- src/app/api/users/blog/route.js
+++ src/app/api/users/blog/route.js
@@ -1,11 +1,112 @@
-import { NextResponse } from 'next/server';
+// import { NextResponse } from 'next/server';
+// import connectDB from '@/db/connection';
+// import BlogPost from '@/models/blogModel';
+// import user from "@/models/signupModel";
+// import cloudinary from '@/utils/cloudinary';
+// import {redisClient, connectRedis} from "@/utils/redis"
+// import { DateTime } from 'luxon';
+// import jwt from 'jsonwebtoken';
+
+// const secretKey = process.env.JWT_SECRET_KEY;
+
+// export async function POST(request) {
+//   try {
+//     const data = await request.formData();
+//     const title = data.get('title');
+//     const blog = data.get('blog');
+//     const category = data.get('category');
+//     const image1File = data.get('image1');
+
+
+//     // if (new Blob([blog]).size > 500000) {
+//     //   return new NextResponse(
+//     //     JSON.stringify({ success: false, error: 'Blog content is too large.' }),
+//     //     { status: 413 }
+//     //   );
+//     // }
+
+//     const buffer1 = await image1File.arrayBuffer();
+//     const image1 = Buffer.from(new Uint8Array(buffer1));
+
+//     // if (image1.length > 2000000) {
+//     //   return new NextResponse(
+//     //     JSON.stringify({ success: false, error: 'Image is too large.' }),
+//     //     { status: 413 }
+//     //   );
+//     // }
+
+
+//     const imageUrl1 = await uploadToCloudinary(image1, image1File.name, image1File.type);
+
+//     const token = request.headers.get('Authorization')?.split(' ')[1];
+//     await connectDB();
+
+//     const decodedToken = jwt.verify(token, secretKey);
+//     const userId = decodedToken.userId;
+//     const author = await user.findOne({_id: userId});
+
+//     const newPost = new BlogPost({
+//       title,
+//       blog,
+//       category,
+//       image1: imageUrl1,
+//       user: {
+//         name: author.name,
+//         profileImage: author.profileImage ? author.profileImage : null,
+//         author: author._id,
+//       },
+//       date: DateTime.now().toLocaleString({ month: '2-digit', day: '2-digit', year: '2-digit' }),
+//     });
+
+//     await newPost.save();
+
+//     await redisClient.del('blog_posts');
+
+//     return new NextResponse(
+//       JSON.stringify({ success: true }),
+//       { status: 200 }
+//     );
+//   } catch (error) {
+//     console.error('Error saving document:', error);
+//     return new NextResponse(
+//       JSON.stringify({ success: false, error: error.message }),
+//       { status: 500 }
+//     );
+//   }
+// }
+
+// function uploadToCloudinary(buffer, fileName, mimeType) {
+//   return new Promise((resolve, reject) => {
+//     const stream = cloudinary.uploader.upload_stream(
+//       {
+//         folder: "blog",
+//         filename_override: fileName,
+//         format: mimeType.split('/').pop(),
+//       },
+//       (error, result) => {
+//         if (error) {
+//           console.error('Error uploading file to Cloudinary:', error);
+//           return reject(error);
+//         }
+//         resolve(result.secure_url);
+//       }
+//     );
+
+//     stream.end(buffer);
+//   });
+// }
+
+
+
 import connectDB from '@/db/connection';
 import BlogPost from '@/models/blogModel';
 import user from "@/models/signupModel";
 import cloudinary from '@/utils/cloudinary';
-import {redisClient, connectRedis} from "@/utils/redis"
+import { redisClient, connectRedis } from "@/utils/redis";
 import { DateTime } from 'luxon';
 import jwt from 'jsonwebtoken';
+import { NextResponse } from 'next/server';
+import { Readable } from 'stream';
 
 const secretKey = process.env.JWT_SECRET_KEY;
 
@@ -13,41 +114,25 @@ export async function POST(request) {
   try {
     const data = await request.formData();
     const title = data.get('title');
-    const blog = data.get('blog');
     const category = data.get('category');
     const image1File = data.get('image1');
 
-
-    // if (new Blob([blog]).size > 500000) {
-    //   return new NextResponse(
-    //     JSON.stringify({ success: false, error: 'Blog content is too large.' }),
-    //     { status: 413 }
-    //   );
-    // }
-
-    const buffer1 = await image1File.arrayBuffer();
-    const image1 = Buffer.from(new Uint8Array(buffer1));
-
-    // if (image1.length > 2000000) {
-    //   return new NextResponse(
-    //     JSON.stringify({ success: false, error: 'Image is too large.' }),
-    //     { status: 413 }
-    //   );
-    // }
-
-
-    const imageUrl1 = await uploadToCloudinary(image1, image1File.name, image1File.type);
+    const blogStream = data.get('blog').stream(); // Get the readable stream for blog content
 
     const token = request.headers.get('Authorization')?.split(' ')[1];
     await connectDB();
 
     const decodedToken = jwt.verify(token, secretKey);
     const userId = decodedToken.userId;
-    const author = await user.findOne({_id: userId});
+    const author = await user.findOne({ _id: userId });
 
+    // Upload image to Cloudinary
+    const imageUrl1 = await uploadToCloudinary(image1File);
+
+    // Save blog post
     const newPost = new BlogPost({
       title,
-      blog,
+      blog: '', // Initialize blog field (will be populated later)
       category,
       image1: imageUrl1,
       user: {
@@ -58,7 +143,8 @@ export async function POST(request) {
       date: DateTime.now().toLocaleString({ month: '2-digit', day: '2-digit', year: '2-digit' }),
     });
 
-    await newPost.save();
+    // Save blog content to MongoDB
+    await saveBlogContent(newPost, blogStream);
 
     await redisClient.del('blog_posts');
 
@@ -75,26 +161,47 @@ export async function POST(request) {
   }
 }
 
-function uploadToCloudinary(buffer, fileName, mimeType) {
+async function uploadToCloudinary(file) {
   return new Promise((resolve, reject) => {
     const stream = cloudinary.uploader.upload_stream(
       {
         folder: "blog",
-        filename_override: fileName,
-        format: mimeType.split('/').pop(),
+        filename_override: file.name,
+        format: file.type.split('/').pop(),
       },
       (error, result) => {
         if (error) {
           console.error('Error uploading file to Cloudinary:', error);
-          return reject(error);
+          reject(error);
+        } else {
+          resolve(result.secure_url);
         }
-        resolve(result.secure_url);
       }
     );
 
-    stream.end(buffer);
+    file.stream.pipe(stream);
   });
 }
+
+async function saveBlogContent(blogPost, blogStream) {
+  // Readable stream to capture blog content
+  const readableStream = Readable.from(blogStream);
+
+  // Buffer to accumulate blog content
+  let blogContent = '';
+
+  // Read chunks of data from the readable stream
+  for await (const chunk of readableStream) {
+    blogContent += chunk.toString();
+  }
+
+  // Set the blog content in the blogPost object
+  blogPost.blog = blogContent;
+
+  // Save the blog post to MongoDB
+  await blogPost.save();
+}
+
 
 
 
